2512327744439fbba3e968b0c2db77c10cd6a86e
Ena Fazlic <ena.fazla@outlook.com>
2022-12-06 15:01:08 +0100

    Prikaz svih knjiga iz baze

---
+++ prikaziKnjige.php
@@ -0,0 +1,236 @@
+ <?php
+include 'konekcija.php';
+include 'model/knjiga.php';
+include 'model/zanr.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php
+if (isset($_POST['zanr'])) {
+  $icko = $_POST['zanr'];
+}
+?>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+  <div class="navbar navbar-inverse navbar-static-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a id="btn-Pocetna" href="index.php" type="button" class="btn btn-success btn-block">Početna</a></li>
+          <li><a id="btn-Dodaj" type="button" class="btn btn-success btn-block" data-toggle="modal" data-target="#my">Dodaj novu knjigu</a></li>
+          <li><a id="btn-prikazi" href="prikaziKnjige.php" type="button" class="btn btn-success btn-block">Prikazi knjige</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="my" role="dialog">
+        <div class="modal-dialog">
+            
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <div class="container prijava-form">
+                        <form action="#" method="post" id="dodajForm">
+                            <h3 style="color: black; text-align: center">Dodaj novu knjigu</h3>
+                            <div class="row">
+                                <div class="col-md-11 ">
+                                    <div class="form-group">
+                                        <label for="">Naziv knjige</label>
+                                        <input type="text" style="border: 1px solid black" name="naziv" class="form-control" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">Autor</label>
+                                        <input type="text" style="border: 1px solid black" name="autor" class="form-control" />
+                                    </div>
+                                    <div class="form-group">
+                                        <select id="zanr" name="zanr" class="form-control">
+                                            <?php
+                                            $rez = $conn->query("SELECT * from zanr");
+                                            while ($red = $rez->fetch_assoc()) {
+                                            ?>
+                                               <option value="<?php echo $red["zanrId"] ?>"> <?php echo $red["nazivZanra"] ?></option>
+                                            <?php  }
+                                            ?>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-12" style="display: none;">
+                                        <div class="form-group">
+                                            <label for="">Datum neki</label>
+                                            <input type="date" style="border: 1px solid black" name="datum" class="form-control" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <button id="btnDodaj" type="submit" class="btn btn-success btn-block" tyle="background-color: orange; border: 1px solid black;">Dodaj</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+  <div class="container pt">
+    <?php
+    $niz = [];
+    $rez = $conn->query("select * from knjiga k join zanr z on k.Zanr=z.ZanrId");
+    while ($red = $rez->fetch_assoc()) {
+      $Zanr = new Zanr($red['zanrId'], $red['nazivZanra']);
+      $knjiga = new Knjiga($red['IdKnjige'], $red['NazivKnjige'], $red['Autor'], $Zanr);
+      array_push($niz, $knjiga);
+    }
+    ?>
+    <p id="p">Lista svih knjiga</p>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th>Naziv knjige</th>
+          <th>Autor knjige</th>
+          <th>Zanr</th>
+          <th>Obriši</th>
+          <th>Izmeni</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        foreach ($niz as $vrednost) {
+        ?>
+          <tr>
+            <td style="display:none" data-target="idZanra"><?php echo $vrednost->Zanr->ZanrId?></td>
+            <td data-target="naziv"><?php echo $vrednost->NazivKnjige ?> </td>
+            <td data-target="autor"><?php echo $vrednost->Autor ?> </td>
+            <td data-target="zanr"><?php echo $vrednost->Zanr->NazivZanra ?></td>
+            <td><button id="btnObrisi" name="btnObrisi" class="btn btn-danger" data-id1="<?php echo $vrednost->IdKnjige ?>">Obriši</a></td>
+            <td><button class="btn btn-info" data-toggle="modal" data-target="#my1" data-id2="<?php echo $vrednost->IdKnjige ?>">Izmeni</a></td>
+          </tr>
+        <?php
+        }
+        ?>
+      </tbody>
+    </table>
+
+  </div>
+
+  <div class="modal fade" id="my1" role="dialog">
+    <div class="modal-dialog">
+      <!-- zakazi modal -->
+      <!--Sadrzaj modala-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body">
+          <div class="container prijava-form">
+            <form action="#" method="post" id="izmeniForma">
+
+
+              <h3 style="color: black; text-align: center">Izmeni podatke o knjizi</h3>
+              <div class="row">
+                <div class="col-md-11 ">
+
+                  <div style="display: none;" class="form-group">
+                    <label for="">IdKnjige</label>
+                    <input  id="idKnjige" type="text" style="border: 1px solid black" name="idKnjige" class="form-control" />
+                  </div>
+
+                  <div class="form-group" style="display: none;">
+                    <label for="">IdZanra</label>
+                    <input id="idZanra"  type="text" style="border: 1px solid black" name="idZanra" class="form-control" />
+                  </div>
+
+
+                  <div class="form-group">
+                    <label for="">Naziv knjige</label>
+                    <input id="naziv" type="text" style="border: 1px solid black" name="naziv" class="form-control" />
+                  </div>
+                  <div class="form-group">
+                    <label for="">Autor</label>
+                    <input id="autor" type="text" style="border: 1px solid black" name="autor" class="form-control" />
+                  </div>
+                  <div class="form-group">
+                    <label for="">Zanr</label>
+
+                    <select title="zanrr" id="zanr" name="zanr" class="form-control">
+                      <?php
+                      $rez = $conn->query("SELECT * from zanr");
+                      while ($red = $rez->fetch_assoc()) {
+                      ?>
+                        <option value="<?php echo $red["zanrId"] ?>"> <?php echo $red["nazivZanra"] ?></option>
+                      <?php  }
+                      ?>
+                    </select>
+                  </div>
+                  <div class="col-md-12" style="display: none;">
+                    <div class="form-group">
+                      <label for="" >Datum neki</label>
+                      <input type="date" style="border: 1px solid black" name="datum" class="form-control" />
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <button id="btnIzmeni" type="submit" class="btn btn-success btn-block" tyle="background-color: orange; border: 1px solid black;">Izmeni</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+  <div id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <h4>Adresa</h4>
+          <p>
+            Milana Rakica 77,<br />
+            0655417876, <br />
+            Beograd, Srbija.
+          </p>
+        </div>
+
+        <div class="col-lg-4">
+          <h4>Društvene mreže</h4>
+          <p>
+            <a href="https://www.facebook.com/fon.bg.ac.rs">Facebook</a><br />
+            <a href="https://twitter.com/fonbg">Twitter</a><br />
+            <a href="http://plus.google.com/106390371419524147048/posts">Google+</a>
+          </p>
+        </div>
+
+      </div>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html> 
